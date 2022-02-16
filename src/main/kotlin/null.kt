90c89518d9b5992b941172297d36bb4225636597
neppord <neppord@gmail.com>
2022-02-16 13:38:00 +0100

    Checkpoint utillity

--- src/main/kotlin/null.kt
+++ src/main/kotlin/null.kt
@@ -1,6 +1,5 @@
-fun <A, B> ((A) -> B).fmap(a: A?): B? = TODO()
+fun <A, B> ((A) -> B).fmap(a: A?): B? = a?.let(this)
 
-fun <A, B> ((A) -> B)?.ap(a: A?): B? = TODO()
+fun <A, B> ((A) -> B)?.ap(a: A?): B? = this?.fmap(a)
 
-fun <A> A?.or(other: A?): A? =
-    TODO("This is part of Alternative, wait with this one until your done with Applicative") 
+fun <A> A?.or(other: A?): A? = this ?: other
--- src/main/kotlin/pair.kt
+++ src/main/kotlin/pair.kt
@@ -1,3 +1,5 @@
 
 fun <A, B, C> Pair<A, B>.map(f: (A) -> C):Pair<C, B> =
     f(first) to second
+
+fun <A, B, C> ((A) -> C).fmap(p :Pair<A, B> ):Pair<C, B> = p.map(this)
