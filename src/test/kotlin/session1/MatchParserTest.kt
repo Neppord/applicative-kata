f2ea7a20c0c60911571a405bcc3f8db457336ded
neppord <neppord@gmail.com>
2022-03-16 13:57:49 +0100

    Add first test

---
+++ src/test/kotlin/session1/MatchParserTest.kt
@@ -0,0 +1,11 @@
+package session1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MatchParserTest {
+    @Test
+    internal fun `it matches its input`() {
+        TODO("Not yet implemented")
+    }
+}
