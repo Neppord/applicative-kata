28da6ad189795416532fdd65186d70f47dbbab82
neppord <neppord@gmail.com>
2022-03-16 13:53:56 +0100

    Add OO start position for Parser

---
+++ src/main/kotlin/session1/language.kt
@@ -0,0 +1,7 @@
+package session1
+
+var oneParser: Parser<String> = TODO("Use MatchParser to build this one")
+/* 2-0 parsers needs to be implemented here*/
+val digitParser: Parser<String> = TODO("Use OrParser to build this one")
+val numberParser: Parser<String> = TODO("Use OneOrMoreParser to build this one")
+val intParser: Parser<Int> = TODO("Use map to build this one")
